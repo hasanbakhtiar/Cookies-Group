46c95f4b08bf6fd4a949d3b39f71183c1a999c01
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-07-22 21:09:20 +0400

    js enter

---
+++ JSLessons/VanillaJS/lesson1-enter/js/script.js
@@ -0,0 +1,19 @@
+//one line
+
+
+/**
+ * more 
+ * line 
+ * comment
+ */
+// document.write('Hello JavaScript');
+
+// debugger;
+// console.log('Hello JS1')
+// console.log('Hello JS2')
+// console.log('Hello JS3')
+// console.log('Hello JS4')
+// console.log('Hello JS5')
+
+// alert("Hello")
+
---
+++ JSLessons/VanillaJS/lesson2-datatypes/js/script.js
@@ -0,0 +1,60 @@
+// var info = "Hello1";
+// console.log(info);
+
+
+// DATA TYPES
+
+// ============================================
+//PRIMITIVE DATA TYPES -> BESIT DATA TIPLERI 
+// string - metn
+// number - reqem
+// boolean - mentiqi
+
+// string DT
+// var infoSt = "Hello";
+// var infoSt1 = "JavaScript";
+// console.log(infoSt+infoSt1);
+
+// number DT
+// var infoNb = 5;
+// var infoNb1 = 15;
+// console.log(infoNb+infoNb1);
+
+// boolean DT
+// true - dogru
+// false - yanlis
+
+// if (false) {
+//     console.log("Bu dogru ifadedir");
+    
+// }else{
+//     console.log("Bu yanlis ifadedir");
+// }
+
+// ============================================
+//COMPOSITE DATA TYPES -> BIRLESIK DATA TIPLERI
+// array
+// object
+
+
+// array DT
+// var infoArr = ["table",1,true,10];
+// var infoArr = new Array("table",1,true,10);
+// console.log(infoArr[2]);
+
+// object DT 
+// var infoObj = {
+//     name:"Emin",
+//     age:24,
+//     surname:"Abdullayev"
+// };
+// console.log(infoObj.age);
+
+// ============================================
+//SPECIAL DATA TYPES -> XUSUSI DATA TIPLERI
+// undefined
+// var a = undefined;
+// console.log(a);
+// null
+// var b = null;
+// console.log(b);
