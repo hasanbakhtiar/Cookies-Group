e6940ac1f956477641cdd7b67fcdbc45b5574898
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-07-30 19:05:42 +0400

    operators

---
+++ JSLessons/VanillaJS/lesson3-opertors/js/script.js
@@ -0,0 +1,97 @@
+// Assigment Operators
+// =
+// +=
+// -=
+// *=
+// **=
+// /=
+// %=
+
+// var numOne = 5;
+// var numTwo = 10;
+// // numOne = numOne + numTwo;
+// numOne **= numTwo;
+// console.log(numOne);
+
+
+
+// ==================================================
+// Arithmetic Operators
+
+// +
+// -
+// /
+// *
+// %
+// **
+// ++
+// --
+
+// var numOne = 5;
+// var numTwo = 10;
+// numOne--;
+
+// console.log(numOne);
+
+// ==================================================
+// Comparison Opertors 
+
+// ==
+// ===
+// <
+// <=
+// >
+// >=
+// != 
+// !==
+
+// var numOne = "10";
+// var numTwo = 10;
+
+// if (numOne !==  numTwo) {
+//     console.log(true);
+// }else{
+//     console.log(false);
+// }
+
+// ==================================================
+// Logical Opertors 
+// && - AND 
+// || - OR
+// ! - NOT
+
+
+
+// if (!true) {
+//     console.log("Duzdu kardes");
+// }else{
+//     console.log("Sehvdi kardes");
+
+// }
+
+// var dataUsername = "EminAbdullayev";
+// var dataPass = "Emin123";
+
+// var inputUsername = "EminAbdullayev";
+// var inputPass = "Emin123";
+
+// if ((dataUsername === inputUsername) && (dataPass === inputPass)) {
+//     console.log('login successfull');
+// }else{
+//     console.log('pass or username wrong...');
+// }
+
+
+// ==================================================
+// String Operators
+// var info = "Java";
+// var infoTwo = "Script";
+// info += infoTwo;
+// console.log(info);
+// console.log(info+infoTwo);
+
+// ==================================================
+// Special Operators
+
+
+
