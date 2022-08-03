a3742fbdca318cebd704631976ac4f6dce1ead19
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-08-03 19:10:49 +0400

    condition

--- JSLessons/VanillaJS/lesson3-opertors/js/script.js
+++ JSLessons/VanillaJS/lesson3-opertors/js/script.js
@@ -94,4 +94,88 @@
 // Special Operators
 
 
+// --------------------------------------------------------------------------------------------------
+// Other (Special) Operators (Xüsusi operatorlar) Start
+
+// ,            (vergül)(kodları bir-birindən ayırmaq və sintaksis qanun uyğunluqları icra etmək üçündür )
+// var a = 5, b = 10;
+// console.log(a,b);
+
+// \            (tərs slash)(Qaçındırma operatorudur)
+// var a = "JavaScript\"s Lessons";
+// console.log(a);
+
+// typeof       (tip üçün)(datanın tipini təyin etmək üçündür)
+// var a = true;
+// console.log(typeof(a));
+
+// ?:           (sual üst-üstə qoşa nöqtə)((şərt) ? ifadə1 : ifadə2)(ternary)
+// şərt            ? true                 : false
+// (5<10) ? console.log("Duzdur"): console.log("Sehvdir");
+
+
+// void         (dəyişkənin dəyərini undefined etmək üçündür)
+// var a = undefined;
+// console.log(a);
+
+// delete       (objectin dəyərini silmək üçündür)
+// var b = {
+    // test:"Hello"
+// };
+// delete b.test;
+
+// console.log(b);
+
+// new          (yeni dəyər yaratməq üçündür)
+// class Human{
+//     constructor(name){
+//         this.ad = name;
+//     }
+//     sayHi(){
+//         return `Hello ${this.ad}`;
+//     }
+// }
+
+// const myHuman = new Human("Shamil");
+// console.log(myHuman.sayHi());
+// this         (mövcud dəyəri işarələmək üçündür)
+
+// in           (dəyişkənin(object) daxilində dəyərin olub olmamasını yoxlamaq üçündür)
+// const car = { make: 'Honda', model: 'Accord', year: 1998 };
+
+// console.log('make' in car);
+// // expected output: true
+
+// delete car.make;
+// if ('make' in car === false) {
+//   car.make = 'Suzuki';
+// }
+
+// console.log(car.make);
+// // expected output: "Suzuki"
+
+// instanceof   (dəyişkənin(objectin xüsusiyyətinin) içində xüsusiyyət olub olmamasını yoxlamaq üçündür)
+
+// function Car(make, model, year) {
+//     this.make = make;
+//     this.model = model;
+//     this.year = year;
+//   }
+//   const auto = new Car('Honda', 'Accord', 1998);
+  
+//   console.log(auto instanceof Car);
+  // expected output: true
+  
+//   console.log(auto instanceof Object);
+  // expected output: true
+  
+
+
+
+    
+
+// Other (Special) Operators (Xüsusi operatorlar) End
+// --------------------------------------------------------------------------------------------------
+
+
 
---
+++ JSLessons/VanillaJS/lesson4-variablesConditions/js/script.js
@@ -0,0 +1,100 @@
+// 1.Dəyişkən adları mənalı və anlaşıqlı olmalıdır.
+// 2.Dəyişkən adlarını yazarkən İngilis əlifbasından başqa əlifadan istifadə etmək qadağandır.
+// 3.Dəyişkən adları _ (alt xətt) ilə başlaya bilər.
+// 4.Dəyişkən adları $(dollar) işarəsi ilə başlaya bilər.
+// 5.Dəyişkən adları heç bir zaman rəqəm ilə başlaya bilməz.Amma ilk hərfdə sonra istifadə edə bilərik.
+// 6.Dəyişkən adları böyük kiçik hərfləri anlayır.
+// 7.Eyni dəyişkənə iki fərqli məlumat tanımlanan zaman sonuncu tanımlanan məlumatı göstərir.
+// 8.JavaScript tərəfindən hazırlanmış rezerv kəlimələri dəyişkən adlarına vermək qadağandır.
+// 9.Eyni dəyişkən adını dəişkən ifadələrində istifadə etmək qadağandır.
+
+// abstract 	    arguments	    await*	            boolean
+// break	        byte	        case	            catch
+// char	            class*	        const	            continue
+// debugger	        default	        delete	            do
+// double	        else	        enum*	            eval
+// export*	        extends*	    false	            final
+// finally	        float	        for	                function
+// goto	            if	            implements	        import*
+// in	            instanceof	    int	                interface
+// let*	            long	        native	            new
+// null	            package	        private	            protected
+// public	        return	        short	            static
+// super*	        switch	        synchronized	    this
+// throw	        throws	        transient	        true
+// try	            typeof	        var	                void
+// volatile	        while	        with	            yield
+
+// var        -> variable (global)
+// let        -> let      (local)
+// const      -> constant (local)
+
+// //global
+// if (true) {
+//     // local global
+//     if (true) {
+//         // local global
+//         if (true) {
+//             // local
+//             var a = 5;
+//         }
+//     }
+// }
+
+// console.log(a);
+
+// ===================================================================================
+
+// if -> eger
+// else ->  deyilse
+// else if -> deyilse eger
+
+// const a = prompt();
+// if (a == 1) {
+//     console.log("a beraberdir 1");
+// }else if(a == 2){
+//     console.log("a beraberdir 2");
+
+// }
+// else if(a == 3){
+//     console.log("a beraberdir 3");
+
+// }
+// else{
+//     console.log("Duz reqem istifade edin");
+// }
+
+// const myTime = 7;
+
+// if (myTime >= 5 && myTime <= 12) {
+//   console.log("Good Morning");
+//   if (myTime == 7) {
+//     console.log("Get up");
+//   }else if(myTime == 9){
+//     console.log("Have breakfast");
+//   }
+// } else if (myTime >= 13 && myTime < 17) {
+//   console.log("Good Afternoon");
+// } else if (myTime >= 17 && myTime < 21) {
+//   console.log("Good Evening");
+// } else if (myTime >= 21 && myTime <= 23) {
+//   console.log("Good Night");
+// } else {
+//   console.log("No Time");
+// }
+
+// const info = 5;
+// switch (info) {
+//   case 5:
+//     console.log("bu 5dir");
+//     if (info < 10) {
+//         console.log("Hello");
+//     }
+//     break;
+//   case 10:
+//     console.log("bu 10dur");
+//     break;
+//   default:
+//     console.log("duzgun reqem daxil edin");
+//     break;
+// }
