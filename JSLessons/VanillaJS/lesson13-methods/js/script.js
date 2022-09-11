73c3e39c09a4b6a61d93921166b97de22577fb0f
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-09-11 12:27:12 +0400

    methods

---
+++ JSLessons/VanillaJS/lesson13-methods/js/script.js
@@ -0,0 +1,45 @@
+
+
+
+
+// const listArr = [];
+// const list = document.querySelector('.list');
+// document.querySelector('button').onclick = ()=>{
+//     // const p = document.createElement('p');
+//     const input = document.querySelector('input');
+//    let a =  listArr.push(`<p>${input.value}</p>`);
+ 
+//     console.log(listArr);
+//     input.value = "";
+//     // list.appendChild(p);
+//     localStorage.setItem('data',listArr);
+// for(x in listArr){
+//     list.innerHTML = listArr[x];
+
+// }
+// }
+
+// for(x in listArr){
+//     const p = document.createElement('p');
+//     p.innerHTML = localStorage.getItem('data')[x].split(',');
+//     list.appendChild(p);
+
+// }
+
+// list.innerHTML = localStorage.getItem('data').split(',');
+
+// list.innerHTML = localStorage.getItem('data');
+// const p = document.createElement('p');
+// const text = document.createTextNode('Hello');
+// p.appendChild(text);
+// list.appendChild(p);
+
+
+const info = ['table','book','pen']
+// info.push('new')
+// info.pop()
+// info.unshift('new')
+// info.shift();
+// console.log(info);
+// https://www.w3schools.com/jsref/jsref_obj_array.asp
+
