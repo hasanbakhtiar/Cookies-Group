fe1df7a3befbc340231ad57f20ce3415ac8ee27b
Hasan Bakhtiar <hasan.doctype@gmail.com>
2022-10-22 18:56:18 +0400

    function

---
+++ JSLessons/RaactJS/lesson8-functioncomponent/src/index.jsx
@@ -0,0 +1,23 @@
+import React, { Component, Fragment, useState } from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+
+
+
+
+
+// function App (){
+//   return(
+//     <h1>Hello</h1>
+//   )
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+reportWebVitals();
